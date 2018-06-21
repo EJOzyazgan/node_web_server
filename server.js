884da98a90c3df733244599cb790eb203f11e0a5
EJ Ozyazgan <ejozyazgan@gmail.com>
2018-06-20 19:19:33 -0700

    HBS Changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerAsyncHelper('getCurrentYear', () => {
+hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
 
