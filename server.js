499546dabdee4002bb9741f211503d2c8c65606d
EJ Ozyazgan <ejozyazgan@gmail.com>
2018-06-20 17:17:14 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const  express = require('express');
+const hbs = require('hbs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: "Maintenance",
+//         currentYear: new Date().getFullYear(),
+//         welcomeMessage: "Not Working"
+//     })
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: "Home Page, Welcome!",
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Welcome to my website"
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: "About",
+        currentYear: new Date().getFullYear()
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Server running port 3000");
+});
